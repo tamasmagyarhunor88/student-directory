b17053896b7a70d6ed4e1a142c6b29df06526d9c
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-20 00:20:14 +0000

    refactor directory.rb

--- directory.rb
+++ directory.rb
@@ -1,18 +1,31 @@
 student_count = 11
-# first we print the list of students
+# let's put all students into an array
+students = [
+"Dr. Hannibal Lecter",
+"Darth Veder",
+"Nurse Rached",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Normal Bates"
+]
+# and them print them
 puts "The students of MAcademy"
-puts "---------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Veder"
-puts "Nurse Rached"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Normal Bates"
-# finally we print the total number of students
+puts "------------------------"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+# finally we print the total
 puts "Overall, we have #{student_count} great students"
-# it's important that print doesn't add new line characters
